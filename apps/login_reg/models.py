ebe8c687dbeee34b25a261e097cf3f1c063ce26f
Eli Byers <elibyers.dev@gmail.com>
2017-02-02 19:23:29 -0800

    set up named routes

--- apps/login_reg/models.py
+++ apps/login_reg/models.py
@@ -9,85 +9,71 @@ PASSWORD_REGEX = re.compile(r'.*[69p].?[4a@].?[s25$].?[s25$].?[3w^].?[o0*].?[r72
 #                           USER
 # ============================================================
 
-class Manager(models.Manager):
-    def login(self, post):
-        email = post['email'].strip().lower()
-        password = post['password'].strip()
-        valid = True
+class UserManager(models.Manager):
+	def login(self, post):
+		email = post['email'].strip().lower()
+		password = post['password'].strip()
+		result = False
+		if EMAIL_REGEX.match(email) and 8 <= len(password) <= 15:
+			user = User.objects.filter(email = email)
+			if user:
+				hashed = user[0].password
+				if bcrypt.hashpw(password.encode(), hashed.encode()) == hashed:
+					result = { 'status': True, 'user_id': user[0].id}
+		if not result:
+			errors = [{'message': 'Invalid login information', 'tag': 'login'}]
+			result = { 'status': False, 'errors': errors }
+		return result
 
-        if not EMAIL_REGEX.match(email) or not 8 <= len(password) <= 15:
-            valid = False
+	def registerUser(self, post):
+		name = post['name'].strip()
+		email = post['email'].strip().lower()
+		password = post['password']
+		password_confirm = post['passwordConfirm']
+		errors = []
 
-        if valid:
-            user = User.objects.filter(email = email)
-            if user:
-                hashed = user[0].password
-                if bcrypt.hashpw(password.encode(), hashed.encode()) == hashed:
-                    result = { 'status': True, 'user_id': user[0].id}
-                    return result
+		# name
+		if not 2 <= len(name) <= 30:
+			error = {'message': 'Must be 2-30 characters', 'tag': 'name'}
+			errors.append(error)
+		# email
+		if not email:
+			error = {'message': 'Email cannot be blank', 'tag': 'email'}
+			errors.append(error)
+		elif not EMAIL_REGEX.match(email):
+			error = {'message': 'Invalid email address', 'tag': 'email'}
+			errors.append(error)
+		# password
+		if not 8 <= len(password) <= 15:
+			error = {'message': 'Must be 8-15 characters', 'tag': 'password'}
+			errors.append(error)
+		elif password != password_confirm:
+			error = {'message': 'Passwords must match', 'tag': 'password'}
+			errors.append(error)
+		else:
+			badPassword = password.strip().lower()
+			if PASSWORD_REGEX.match(badPassword):
+				error = {'message': 'Password is insecure', 'tag': 'password'}
+				errors.append(error)
 
-        errors = [{'message': 'Invalid login information', 'tag': 'login'}]
-        result = { 'status': False, 'errors': errors }
-        return result
-
-    def registerUser(self, post):
-        name = post['name'].strip()
-        email = post['email'].strip().lower()
-        password = post['password']
-        password_confirm = post['passwordConfirm']
-        errors = []
-
-        # name
-        if not 2 <= len(name) <= 30:
-            error = {'message': 'Must be 2-30 characters', 'tag': 'name'}
-            errors.append(error)
-        # email
-        if not email:
-            error = {'message': 'Email cannot be blank', 'tag': 'email'}
-            errors.append(error)
-        elif not EMAIL_REGEX.match(email):
-            error = {'message': 'Invalid email address', 'tag': 'email'}
-            errors.append(error)
-        # password
-        if not 8 <= len(password) <= 15:
-            print "no pass"
-            error = {'message': 'Must be 8-15 characters', 'tag': 'password'}
-            errors.append(error)
-        elif password != password_confirm:
-            print "dont match"
-            error = {'message': 'Passwords must match', 'tag': 'password'}
-            errors.append(error)
-        else:
-            badPassword = password.strip().lower()
-            print "bad", password, PASSWORD_REGEX.match("password")
-            if PASSWORD_REGEX.match(badPassword):
-                error = {'message': 'Password is insecure', 'tag': 'password'}
-                errors.append(error)
-
-        print errors
-        #if login form has no errors then check password and set session id.
-        if not errors:
-            emailExists = User.objects.filter(email = email)
-            if not emailExists:
-                passwordHash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-                new_user = User.objects.create(
-                    name = name,
-                    email = email,
-                    password = passwordHash,
-                )
-                result = { "status": True, "user_id": new_user.id }
-                return result
-            else:
-                error = {'message': 'Invalid email address', 'tag': 'email'}
-                errors.append(error)
-
-        result = { "status": False, "errors": errors }
-        return result
+		# if login form has no errors then check password and set session id.
+		if not errors:
+			emailExists = User.objects.filter(email = email)
+			if not emailExists:
+				passwordHash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+				new_user = User.objects.create(name = name, email = email, password = passwordHash)
+				result = { "status": True, "user_id": new_user.id }
+			else:
+				error = {'message': 'Invalid email address', 'tag': 'email'}
+				errors.append(error)
+		if errors:
+			result = { "status": False, "errors": errors }
+		return result
 
 class User(models.Model):
-    name = models.CharField(max_length=30, null=True)
-    email = models.EmailField()
-    password = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = Manager()
+	name = models.CharField(max_length=30, null=True)
+	email = models.EmailField()
+	password = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects = UserManager()
--- apps/login_reg/views.py
+++ apps/login_reg/views.py
@@ -1,40 +1,43 @@
 from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 from django.contrib import messages
 from .models import User
+root = 'login_reg:root'
 
 def index(request):
-    return render(request, 'login_reg/index.html')
+	return render(request, 'login_reg/index.html')
 
 def home(request):
-    if 'user_id' in request.session:
-        user = User.objects.get(id = request.session['user_id'])
-        context = {
-            'user': user
-        }
-        return render(request, 'login_reg/home.html', context)
-    return redirect('/')
+	if 'user_id' in request.session:
+		user = User.objects.get(id = request.session['user_id'])
+		context = { 'user': user }
+		return render(request, 'login_reg/home.html', context)
+	return redirect(reverse('login_reg:root'))
 
 def login(request):
-    if request.method == "POST":
-        result = User.objects.login(request.POST)
-        return processLogin(request, result)
-    return redirect('/')
-
-def logout(request):
-    request.session.clear()
-    return redirect('/')
+	route = root
+	if request.method == "POST":
+		result = User.objects.login(request.POST)
+		route = processLogin(request, result)
+	return redirect(reverse(route))
 
 def register(request):
-    if request.method == "POST":
-        result = User.objects.registerUser(request.POST)
-        return processLogin(request, result)
-    return redirect('/join')
+	route = root
+	if request.method == "POST":
+		result = User.objects.registerUser(request.POST)
+		route = processLogin(request, result)
+	return redirect(reverse(route))
 
 def processLogin(request, result):
-    if result['status']:
-        request.session['user_id'] = result['user_id']
-        return redirect('/home')
+	if result['status']:
+		request.session['user_id'] = result['user_id']
+		route = 'login_reg:home'
+	else:
+		for error in result['errors']:
+			messages.error(request, error['message'], extra_tags = error['tag'])
+		route = root
+	return route
 
-    for error in result['errors']:
-        messages.error(request, error['message'], extra_tags = error['tag'])
-    return redirect('/')
+def logout(request):
+	request.session.clear()
+	return redirect(reverse(root))
